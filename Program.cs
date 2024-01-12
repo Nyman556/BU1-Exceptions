fd5609ef1181ae3d533f5c5482633a7dd432d85d
Johan Nyman <johan.nyman97@gmail.com>
2024-01-13 00:36:47 +0100

    Övning 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace exceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        if(Övning1(input, out int result))
+        {
+            Console.WriteLine($"Parsed: {result}");
+        } else 
+        {
+            Console.WriteLine("Fel Input. Skriv ett nummer");
+        }
+
+        bool Övning1(string input, out int number) 
+        {
+            try
+            {
+                number = Int32.Parse(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                number = 0;
+                return false;
+            }
+        }
+    }
+}
